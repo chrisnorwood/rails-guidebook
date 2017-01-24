ed3c08b9b18067294b90b9d07a727ee2fa795b07
Chris Norwood <cnorwood92@gmail.com>
2017-01-24 10:46:31 -0800

    Refactoring conditionals in scrape_climb

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -25,22 +25,18 @@ class Scraper < ApplicationRecord
       scraped_area.save
 
       area_links = page.search('#viewerLeftNavColContent .roundedTop div a[target=_top]')
-      # area_links = page.links_with(href: /\/v\/+.*/)
 
       if !area_links.empty?
         # We are in an area with sub-areas,
-        # SO CLICK LINKS, and recursively call this function (with agent context) on it to Save Area info
+        # So recursively call this function to save subarea info
         area_links.each do |area_link|
-          # p area_link.attribute('href').value
           built_url = 'https://www.mountainproject.com' + area_link.attribute('href').value
           scrape_areas(built_url, agent, scraped_area.id)
         end
       else
         # We are in an area that has CLIMBS (NO sub-areas)
-
         climbs_links = page.search('#leftNavRoutes tr td a')
         climbs_links.each do |climb_link|
-          # p climb_link.attribute('href').value
           built_url = 'https://www.mountainproject.com' + climb_link.attribute('href').value
           scrape_climb(built_url, agent, scraped_area.id)
         end
@@ -52,30 +48,24 @@ class Scraper < ApplicationRecord
     def scrape_climb(mp_climb_url, agent=Mechanize.new, area_id=1)
       page = agent.get(mp_climb_url)
 
-      climb_name = page.search('.rspCol h1.dkorange em span[itemprop=itemreviewed]').children.first.content
-
-      climb_grade = if res = page.search('.rateYDS').children[2]
-                      res.content.gsub(/\A[[:space:]]+|[[:space:]]+\z/, '')
-                    elsif res = page.search('.rateHueco').children[2]
-                      res.content.gsub(/\A[[:space:]]+|[[:space:]]+\z/, '')
-                    end
-
-      climb_type = if res = page.search('.rspCol span table tr:first-child td:nth-child(2)').children.first
-                     page.search('.rspCol span table tr:first-child td:nth-child(2)').children.first.content
-                   end
-
-      climb_fa = if res = page.search('.rspCol span table tr:nth-child(3) td:nth-child(2)').children[1]
-                   res.content
-                 end
-
-      climb_desc = if res = page.search('h3.dkorange + div').children[1]
-                     res.content
-                   end
-
-      climb_location = if res = page.search('h3.dkorange + div').children[4]
-                         res.content
-                       end
-
+      climb_name       = page.search('.rspCol h1.dkorange em span[itemprop=itemreviewed]').children.first.content
+      climb_grade      = if res = page.search('.rateYDS').children[2]
+                           res.content.gsub(/\A[[:space:]]+|[[:space:]]+\z/, '')
+                         elsif res = page.search('.rateHueco').children[2]
+                           res.content.gsub(/\A[[:space:]]+|[[:space:]]+\z/, '')
+                         end
+      climb_type       = if res = page.search('.rspCol span table tr:first-child td:nth-child(2)').children.first
+                           res.content
+                         end
+      climb_fa         = if res = page.search('.rspCol span table tr:nth-child(3) td:nth-child(2)').children[1]
+                           res.content
+                         end
+      climb_desc       = if res = page.search('h3.dkorange + div').children[1]
+                           res.content
+                         end
+      climb_location   = if res = page.search('h3.dkorange + div').children[4]
+                           res.content
+                         end
       climb_protection = if res = page.search('h3.dkorange + div').children[7]
                            res.content.gsub(/\A[[:space:]]+|[[:space:]]+\z/, '')
                          end
